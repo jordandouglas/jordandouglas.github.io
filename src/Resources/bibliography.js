c48ceb8cdc9a45581b465c3a7fbd036a1e81ec50
jordandouglas <31677293+jordandouglas@users.noreply.github.com>
2017-09-19 19:55:35 +1200

    Add files via upload

--- src/Resources/bibliography.js
+++ src/Resources/bibliography.js
@@ -48,7 +48,77 @@ BIBLIOGRAPHY["kuhner2007force"] = `Kühner, Ferdinand, et al. "Force-induced DNA
 BIBLIOGRAPHY["neher2004dynamics"] = `Neher, Richard A., and Ulrich Gerland. "Dynamics of force-induced DNA slippage." Physical review letters 93.19 (2004): 198102.`;
 
 
+BIBLIOGRAPHY["abbondanzieri2005direct"] = `Abbondanzieri, Elio A., et al. "Direct observation of base-pair stepping by RNA polymerase." Nature 438.7067 (2005): 460.`;
+BIBLIOGRAPHY["neuman2003ubiquitous"] = `Neuman, Keir C., et al. "Ubiquitous transcriptional pausing is independent of RNA polymerase backtracking." Cell 115.4 (2003): 437-447.`;
+BIBLIOGRAPHY["davenport2000single"] = `Davenport, R. John, et al. "Single-molecule study of transcriptional pausing and arrest by E. coli RNA polymerase." Science 287.5462 (2000): 2497.`;
+BIBLIOGRAPHY["bai2007mechanochemical"] = `Bai, Lu, Robert M. Fulbright, and Michelle D. Wang. "Mechanochemical kinetics of transcription elongation." Physical review letters 98.6 (2007): 068103.`;
+BIBLIOGRAPHY["mejia2015trigger"] = `Mejia, Yara X., Evgeny Nudler, and Carlos Bustamante. "Trigger loop folding determines transcription rate of Escherichia coli’s RNA polymerase." Proceedings of the National Academy of Sciences 112.3 (2015): 743-748.`;
+BIBLIOGRAPHY["richardson1996control"] = `Richardson, John P., and Jack Greenblatt. "Control of RNA chain elongation and termination." Escherichia coli and Salmonella: cellular and molecular biology 1 (1996): 822-848.`;
+BIBLIOGRAPHY["iost1992bacteriophage"] = `Iost, I., J. Guillerez, and M. Dreyfus. "Bacteriophage T7 RNA polymerase travels far ahead of ribosomes in vivo." Journal of bacteriology 174.2 (1992): 619-622.`;
+BIBLIOGRAPHY["vogel1994rna"] = `Vogel, Ulla, and Kaj Frank Jensen. "The RNA chain elongation rate in Escherichia coli depends on the growth rate." Journal of bacteriology 176.10 (1994): 2807-2813.`;
+BIBLIOGRAPHY["bremer1996modulation"] = `Bremer, H. D. P. P., and Patrick P. Dennis. "Modulation of chemical composition and other parameters of the cell by growth rate." Escherichia coli and Salmonella: cellular and molecular biology 2.2 (1996): 1553-69.`;
+BIBLIOGRAPHY["ryals1982temperature"] = `Ryals, J., R. Little, and H. Bremer. "Temperature dependence of RNA synthesis parameters in Escherichia coli." Journal of bacteriology 151.2 (1982): 879-887.`;
+BIBLIOGRAPHY["tennyson1995human"] = `Tennyson, Christine N., Henry J. Klamut, and Ronald G. Worton. "The human dystrophin gene requires 16 hours to be transcribed and is cotranscriptionally spliced." Nature genetics 9.2 (1995): 184-190.`;
+BIBLIOGRAPHY["singh2009rates"] = `Singh, Jarnail, and Richard A. Padgett. "Rates of in situ transcription and splicing in large human genes." Nature structural & molecular biology 16.11 (2009): 1128-1133.`;
+BIBLIOGRAPHY["boireau2007transcriptional"] = `Boireau, Stéphanie, et al. "The transcriptional cycle of HIV-1 in real-time and live cells." J Cell Biol 179.2 (2007): 291-304.`;
+BIBLIOGRAPHY["darzacq2007vivo"] = `Darzacq, Xavier, et al. "In vivo dynamics of RNA polymerase II transcription." Nature structural & molecular biology 14.9 (2007): 796-806.`;
+BIBLIOGRAPHY["galburt2009single"] = `Galburt, Eric A., Stephan W. Grill, and Carlos Bustamante. "Single molecule transcription elongation." Methods 48.4 (2009): 323-332.`;
+BIBLIOGRAPHY["dangkulwanich2013complete"] = `Dangkulwanich, Manchuta, et al. "Complete dissection of transcription elongation reveals slow translocation of RNA polymerase II in a linear ratchet mechanism." Elife 2 (2013): e00971.`;
+BIBLIOGRAPHY["mason2005distinction"] = `Mason, Paul B., and Kevin Struhl. "Distinction and relationship between elongation rate and processivity of RNA polymerase II in vivo." Molecular cell 17.6 (2005): 831-840.`;
+BIBLIOGRAPHY["kovs2010yeast"] = `Koš, Martin, and David Tollervey. "Yeast pre-rRNA processing and modification occur cotranscriptionally." Molecular cell 37.6 (2010): 809-820.`;
+BIBLIOGRAPHY["french2003exponentially"] = `French, Sarah L., et al. "In exponentially growing Saccharomyces cerevisiae cells, rRNA synthesis is determined by the summed RNA polymerase I loading rate rather than by the number of active genes." Molecular and cellular biology 23.5 (2003): 1558-1568.`;
+BIBLIOGRAPHY["sehgal19765"] = `Sehgal, Pravinkumar B., et al. "5, 6-Dichloro-1-Beta-D-ribofuranosylbenzimidazole inhibits initiation of nuclear heterogeneous RNA chains in HeLa cells." Science 194.4263 (1976): 431-433.`;
+BIBLIOGRAPHY["ucker1984early"] = `Ucker, D. S., and K. R. Yamamoto. "Early events in the stimulation of mammary tumor virus RNA synthesis by glucocorticoids. Novel assays of transcription rates." Journal of Biological Chemistry 259.12 (1984): 7416-7420.`;
+BIBLIOGRAPHY["mejia2015trigger"] = `Mejia, Yara X., Evgeny Nudler, and Carlos Bustamante. "Trigger loop folding determines transcription rate of Escherichia coli’s RNA polymerase." Proceedings of the National Academy of Sciences 112.3 (2015): 743-748.`;
+BIBLIOGRAPHY["thummel1990spatial"] = `Thummel, Carl S., Kenneth C. Burtis, and David S. Hogness. "Spatial and temporal patterns of E74 transcription during Drosophila development." Cell 61.1 (1990): 101-111.`;
+BIBLIOGRAPHY["o1993rapid"] = `O'Brien, T. H. O. M. A. S., and John T. Lis. "Rapid changes in Drosophila transcription after an instantaneous heat shock." Molecular and cellular biology 13.6 (1993): 3456-3463.`;
+BIBLIOGRAPHY["shermoen1991progression"] = `Shermoen, Antony W., and Patrick H. O'Farrell. "Progression of the cell cycle through mitosis leads to abortion of nascent transcripts." Cell 67.2 (1991): 303-310.`;
+BIBLIOGRAPHY["yao2007intranuclear"] = `Yao, Jie, et al. "Intranuclear distribution and local dynamics of RNA polymerase II during transcription activation." Molecular cell 28.6 (2007): 978-990.`;
+BIBLIOGRAPHY["dulin2015elongation"] = `Dulin, David, et al. "Elongation-competent pauses govern the fidelity of a viral RNA-dependent RNA polymerase." Cell reports 10.6 (2015): 983-992.`;
+BIBLIOGRAPHY["kainov2004packaging"] = `Kainov, Denis E., et al. "Packaging motor from double-stranded RNA bacteriophage ϕ12 acts as an obligatory passive conduit during transcription." Nucleic acids research 32.12 (2004): 3515-3521.`;
+BIBLIOGRAPHY["skinner2004promoter"] = `Skinner, Gary M., et al. "Promoter binding, initiation, and elongation by bacteriophage T7 RNA polymerase a single-molecule view of the transcription cycle." Journal of Biological Chemistry 279.5 (2004): 3239-3244.`;
+BIBLIOGRAPHY["thomen2008t7"] = `Thomen, Philippe, et al. "T7 RNA polymerase studied by force measurements varying cofactor concentration." Biophysical journal 95.5 (2008): 2423-2433.`;
+BIBLIOGRAPHY["iost1992bacteriophage"] = `Iost, I., J. Guillerez, and M. Dreyfus. "Bacteriophage T7 RNA polymerase travels far ahead of ribosomes in vivo." Journal of bacteriology 174.2 (1992): 619-622.`;
+BIBLIOGRAPHY["anand2006transient"] = `Anand, Vasanti Subramanian, and Smita S. Patel. "Transient state kinetics of transcription elongation by T7 RNA polymerase." Journal of Biological Chemistry 281.47 (2006): 35677-35685.`;
+BIBLIOGRAPHY["makarova1995transcribing"] = `Makarova, Olga V., et al. "Transcribing of Escherichia coli genes with mutant T7 RNA polymerases: stability of lacZ mRNA inversely correlates with polymerase speed." Proceedings of the National Academy of Sciences 92.26 (1995): 12250-12254.`;
+BIBLIOGRAPHY["gubbay2001sendai"] = `Gubbay, Oliver, Joseph Curran, and Daniel Kolakofsky. "Sendai virus genome synthesis and assembly are coupled: a possible mechanism to promote viral RNA polymerase processivity." Journal of General Virology 82.12 (2001): 2895-2903.`;
+BIBLIOGRAPHY["plumet2005dynamics"] = `Plumet, Sébastien, W. Paul Duprex, and Denis Gerlier. "Dynamics of viral RNA synthesis during measles virus infection." Journal of virology 79.11 (2005): 6900-6908.`;
+BIBLIOGRAPHY["iverson1981localized"] = `Iverson, Linda E., and John K. Rose. "Localized attenuation and discontinuous synthesis during vesicular stomatitis virus transcription." Cell 23.2 (1981): 477-484.`;
+
 BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
+BIBLIOGRAPHY["XXX"] = ``;
+
 BIBLIOGRAPHY["XXX"] = ``;
 
 
