4aac896dc594c7197ac91bdb46a5fffcc50a39ea
jordandouglas <31677293+jordandouglas@users.noreply.github.com>
2018-09-26 14:37:40 +1200

    Add files via upload

--- phylopause/src/Controller/PhyloTree.js
+++ phylopause/src/Controller/PhyloTree.js
@@ -23,6 +23,28 @@
 
 
 
+// Upload a tree from a URL
+function uploadTreeFromURL(url, resolve = function() { }){
+    
+    console.log("Trying to open", url);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          
+            if (xhttp == null || xhttp.responseXML == "") return;
+            
+            parseTree(xhttp.responseText, resolve);
+           
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+    
+    
+}
+
+
+
 // Upload a tree
 function uploadTree(fileLocation = null){
     
@@ -61,7 +83,7 @@ function uploadTree(fileLocation = null){
 
 
 
-function parseTree(nexus_str){
+function parseTree(nexus_str, resolve = function() { }){
 
 
 
@@ -84,6 +106,8 @@ function parseTree(nexus_str){
 
             // Draw the tree
             renderTree(parseResult.newick); 
+
+            resolve();
 
         }
 
